d1a7e6f3c67539d5168b82274e47ff6cd9aa9bc5
Chris Athanas <chris@crowdskout.com>
2021-11-25 23:28:00 -0600

    Pre migrate to AndroidX

--- app/src/main/java/com/realityexpander/blitter/MainActivity.kt
+++ app/src/main/java/com/realityexpander/blitter/MainActivity.kt
@@ -3,6 +3,9 @@ package com.realityexpander.blitter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Firebase dashboard
+// https://console.firebase.google.com/u/3/project/blitter-7f038/storage/blitter-7f038.appspot.com/rules
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
