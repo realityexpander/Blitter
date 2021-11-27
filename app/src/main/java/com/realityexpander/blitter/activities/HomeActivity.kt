bb7539281ac7ed5f621e28a297eac003f734c3e6
Chris Athanas <chris@crowdskout.com>
2021-11-27 00:55:52 -0600

    Add Page Scroll Animation

--- app/src/main/java/com/realityexpander/blitter/activities/HomeActivity.kt
+++ app/src/main/java/com/realityexpander/blitter/activities/HomeActivity.kt
@@ -49,6 +49,8 @@ class HomeActivity : AppCompatActivity() {
                 sectionPageAdapter.selectTab(position) // update the tabs for the newly swiped-to fragment
             }
         })
+        // Add page transformer: https://developer.android.com/training/animation/screen-slide-2#pagetransformer
+        bind.viewPager.setPageTransformer(ZoomOutPageTransformer())
 
        // // example to Rename the tabs
        // bind.tabLayout.getTabAt(TabLayoutItem.HOME.ordinal)!!.text = "Home"
@@ -105,5 +107,49 @@ class HomeActivity : AppCompatActivity() {
         super.onPause()
 
         bind.tabLayout.removeOnTabSelectedListener(onTabSelectedListener)
+    }
+
+}
+
+private const val MIN_SCALE = 0.85f
+private const val MIN_ALPHA = 0.5f
+
+class ZoomOutPageTransformer : ViewPager2.PageTransformer {
+
+    override fun transformPage(view: View, position: Float) {
+        view.apply {
+            val pageWidth = width
+            val pageHeight = height
+            when {
+                position < -1 -> { // [-Infinity,-1)
+                    // This page is way off-screen to the left.
+                    alpha = 0f
+                }
+                position <= 1 -> { // [-1,1]
+                    // Modify the default slide transition to shrink the page as well
+                    val scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position))
+                    val vertMargin = pageHeight * (1 - scaleFactor) / 2
+                    val horzMargin = pageWidth * (1 - scaleFactor) / 2
+                    translationX = if (position < 0) {
+                        horzMargin - vertMargin / 2
+                    } else {
+                        horzMargin + vertMargin / 2
+                    }
+
+                    // Scale the page down (between MIN_SCALE and 1)
+                    scaleX = scaleFactor
+                    scaleY = scaleFactor
+                    rotation = position * 360.0f
+
+                    // Fade the page relative to its size.
+                    alpha = (MIN_ALPHA +
+                            (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA)))
+                }
+                else -> { // (1,+Infinity]
+                    // This page is way off-screen to the right.
+                    alpha = 0f
+                }
+            }
+        }
     }
 }
